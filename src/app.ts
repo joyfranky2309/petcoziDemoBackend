7d13ce23992564b73ec266999cdb1729350bc049
joyfranky2309 <joyfranksebastian23@gmail.com>
2024-12-26 21:09:52 +0530

    first commit

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { Application } from 'express';
+import mongoose from 'mongoose';
+const app: Application = express();
+const PORT = 4000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Hello I'm a server");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
